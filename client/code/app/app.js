0027651821418e8c891a00fbca5b3b3774c91fe0
Kim Lokoy <kim.lokoy@gmail.com>
2012-09-21 13:27:31 +0200

    Initial commit.  Working demo but no styling.

---
+++ client/code/app/app.js
@@ -0,0 +1,21 @@
+
+ChatCtrl = function($scope) {
+
+    $scope.messages = [];
+
+    ss.event.on('newMessage', function(message) {
+        $scope.$apply($scope.messages.push(message));
+    });
+
+    $scope.postMessage = function () {
+        ss.rpc('demo.sendMessage', $scope.myMessage, function (a) {
+            if(a) {
+                console.log('message posted!');
+            } else {
+                console.log('something failed!');
+            }
+        });
+        $scope.myMessage = '';
+    };
+}
+
